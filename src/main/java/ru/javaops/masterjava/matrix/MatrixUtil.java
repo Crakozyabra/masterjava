eb40405cd127da009d16c626501d35bcdb4a79d0
Crakozyabra <mrcrakozyabra@gmail.com>
2024-05-26 13:33:35 +0300

    task complete

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -13,47 +13,26 @@ import java.util.concurrent.Future;
  */
 public class MatrixUtil {
 
-    // TODO implement parallel multiplication matrixA*matrixB
+    // implement parallel multiplication matrixA*matrixB
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
-        List<Future<?>> futures = new ArrayList<>();
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int k = i;
-                int m = j;
-                Runnable task = () -> {
-                    for (int n = 0; n < matrixSize; n++) {
-                        matrixC[k][m] += matrixA[k][n] * matrixB[n][m];
-                    }
-                };
-                futures.add(executor.submit(task));
-            }
-        }
-        for (Future<?> future : futures) {
-            future.get();
+        List<Future<?>> tasks = new ArrayList<>();
+        for (int j = 0; j < matrixSize; j++) {
+            int n = j;
+            tasks.add(executor.submit(() -> rowAndColumnMultiply(matrixA, matrixB, matrixSize, matrixC, n)));}
+        for (Future<?> task : tasks) {
+            task.get();
         }
         return matrixC;
     }
 
-    // TODO optimize by https://habrahabr.ru/post/114797/
+    // optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
-        int[][] BT = new int[matrixSize][matrixSize];
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                BT[j][i] = matrixB[i][j];
-            }
-        }
         final int[][] matrixC = new int[matrixSize][matrixSize];
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
-                for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * BT[j][k];
-                }
-                matrixC[i][j] = sum;
-            }
+        for (int j = 0; j < matrixSize; j++) {
+            rowAndColumnMultiply(matrixA, matrixB, matrixSize, matrixC, j);
         }
         return matrixC;
     }
@@ -80,5 +59,20 @@ public class MatrixUtil {
             }
         }
         return true;
+    }
+
+    private static void rowAndColumnMultiply(int[][] matrixA, int[][] matrixB, int matrixSize, int[][] matrixC, int n) {
+        int[] thatColumn = new int[matrixSize];
+        for (int k = 0; k < matrixSize; k++) {
+            thatColumn[k] = matrixB[k][n];
+        }
+        for (int i = 0; i < matrixSize; i++) {
+            int[] thisRow = matrixA[i];
+            int summand = 0;
+            for (int k = 0; k < matrixSize; k++) {
+                summand += thisRow[k] * thatColumn[k];
+            }
+            matrixC[i][n] = summand;
+        }
     }
 }
