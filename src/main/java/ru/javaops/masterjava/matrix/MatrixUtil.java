85332633418e61843e624d260e1cd594e69a6cd0
Crakozyabra <mrcrakozyabra@gmail.com>
2024-05-26 12:56:31 +0300

    optimise single thread, make 1_000_000 tasks in concurrent thread

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,11 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 /**
  * gkislin
@@ -14,20 +17,40 @@ public class MatrixUtil {
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
-
+        List<Future<?>> futures = new ArrayList<>();
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                int k = i;
+                int m = j;
+                Runnable task = () -> {
+                    for (int n = 0; n < matrixSize; n++) {
+                        matrixC[k][m] += matrixA[k][n] * matrixB[n][m];
+                    }
+                };
+                futures.add(executor.submit(task));
+            }
+        }
+        for (Future<?> future : futures) {
+            future.get();
+        }
         return matrixC;
     }
 
     // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
+        int[][] BT = new int[matrixSize][matrixSize];
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                BT[j][i] = matrixB[i][j];
+            }
+        }
         final int[][] matrixC = new int[matrixSize][matrixSize];
-
         for (int i = 0; i < matrixSize; i++) {
             for (int j = 0; j < matrixSize; j++) {
                 int sum = 0;
                 for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+                    sum += matrixA[i][k] * BT[j][k];
                 }
                 matrixC[i][j] = sum;
             }
