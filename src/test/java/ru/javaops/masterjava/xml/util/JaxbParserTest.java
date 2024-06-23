42c707f65feea526f8c3d53c810b8b07487e2f54
Crakozyabra <mrcrakozyabra@gmail.com>
2024-06-23 21:42:25 +0300

    intermediate commit (add generated java - from plugin, make email attribute)

--- src/test/java/ru/javaops/masterjava/xml/util/JaxbParserTest.java
+++ src/test/java/ru/javaops/masterjava/xml/util/JaxbParserTest.java
@@ -2,9 +2,9 @@ package ru.javaops.masterjava.xml.util;
 
 import com.google.common.io.Resources;
 import org.junit.Test;
-import ru.javaops.masterjava.xml.schema.CityType;
-import ru.javaops.masterjava.xml.schema.ObjectFactory;
-import ru.javaops.masterjava.xml.schema.Payload;
+import ru.javaops.masterjava.xml.schemawithplugin.CityType;
+import ru.javaops.masterjava.xml.schemawithplugin.ObjectFactory;
+import ru.javaops.masterjava.xml.schemawithplugin.Payload;
 
 import javax.xml.bind.JAXBElement;
 import javax.xml.namespace.QName;
